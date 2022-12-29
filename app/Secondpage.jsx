652fb45165bac1d4222c0f10639d089b2a1028a2
thatbeer <6130812021@student.chula.ac.th>
2022-12-29 14:51:46 +0700

    update nav bar

--- app/Secondpage.jsx
+++ app/Secondpage.jsx
@@ -3,8 +3,7 @@ import Progressbar from '../components/progressbar'
 import TableContent from "../components/tablecotent";
 import Filtergroup from '../components/filtergroup';
 import FiltergroupMB from '../components/filtergroup(mb)'
-
-
+import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline';
 
 
 const Secondpage = () => {
@@ -65,21 +64,45 @@ const Secondpage = () => {
                     </div>
                 </div> */}
 
-                <div className='fixed top-1 w-screen items-center mx-auto bg-green-200 h-[40px] px-2 py-3 z-30 '>
-                    <div className='flex justify-between items-center bg-red-200'>
-                        <div>sdsdsd</div>
-                        <div>sdsdsd</div>
+                <div className='fixed top-12 w-screen items-center mx-auto h-[50px] z-30 '>
+                    <div className='flex flex-col lg:flex-row mx-auto items-center bg-blue-200'>
+                        {/* <div className='justify-start flex lg:hidden '>
+                            <button className='mx-auto btn btn-warning'>filter1</button>
+                          
+                        </div> */}
+                        <div className=' w-1/3 justify-center hidden lg:flex mx-auto py-1 '>
+                            <button className='mx-10 bg-gray-200 border border-gray-400 py-3 px-6 rounded-lg'>filter1</button>
+                            <button className='mx-10 bg-gray-200 border border-gray-400 py-3 px-6 rounded-lg'>filter2</button>
+                            <button className='mx-10 bg-gray-200 border border-gray-400 py-3 px-6 rounded-lg'>filter3</button>
+                            <button className='mx-10 bg-gray-200 border border-gray-400 py-3 px-6 rounded-lg'>filter4</button>
+
+                        </div>
+                        {/* search bar section */}
+                        <div className='relative flex  text-gray-700 mx-auto lg:mx-2 py-1'>
+                            <input type={'search'} name="search" placeholder='search'
+                                className='border-2 lg:w-screen  border-gray-300 bg-white h-8  px-1 rounded-lg text-sm focus:outline-none'/>
+                            <button type='submit' className='relative '>
+                                <MagnifyingGlassCircleIcon onClick={() => console.log('click search')} className='w-8 h-8 ml-1 hover:text-white' />
+                            </button>
+                        </div>
+
+                        <div className=' justify-between flex lg:hidden mx-auto my-1 '>
+                            <button className='mx-1 bg-gray-200 border border-gray-400 py-2 px-4 rounded-lg'>filter1</button>
+                            <button className='mx-1 bg-gray-200 border border-gray-400 py-2 px-4 rounded-lg'>filter2</button>
+                            <button className='mx-1 bg-gray-200 border border-gray-400 py-2 px-4 rounded-lg'>filter3</button>
+                            <button className='mx-1 bg-gray-200 border border-gray-400 py-2 px-4 rounded-lg'>filter4</button>
+
+                        </div>
 
 
                     </div>
                 </div>
+                {/* <div className='fixed top-4 w-screen items-center mx-auto bg-green-200 h-[200px] px-2 py-3 z-30 '>
+                    
+                </div> */}
 
 
                 <div className='flex flex-1 mx-auto'>
-                    <header>
-
-
-                    </header>
                     <main role="main" className="lg:w-full w-screen h-full flex-grow px-2 ">
                         <div className='flex flex-col lg:flex-row justify-between pt-[64px] mx-auto'>
                             
